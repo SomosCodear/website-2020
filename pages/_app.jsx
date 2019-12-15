6fd4933a25d8a59f64309f9429606fab1eadb5e8
Agustin Carrasco <asermax@gmail.com>
2019-12-15 18:12:32 -0300

    feat(site): add ga tracking

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,6 @@
 import '@codear/lilac/dist/lilac.css';
 import App from 'next/app';
+import Router from 'next/router';
+import withGA from 'next-ga';
 
-export default App;
+export default withGA('UA-148017514-2', Router)(App);

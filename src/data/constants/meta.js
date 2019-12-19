b544f8ff1465e2ad1c446af26bdeee74132128f4
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-12-19 15:15:45 -0300

    added META constants

---
+++ src/data/constants/meta.js
@@ -0,0 +1,13 @@
+export const META = {
+  description: 'La primera conferencia de tecnologías Web del interior del país, presentada por CoDeAr. Te esperamos el 29 y 30 de Mayo de 2020, en el Auditorio de la UTN - Facultad Regional Córdoba. ¡Estamos buscando disertantes!',
+  ogTitle: 'Córdoba WebConf 2020 | ¡Estamos buscando disertantes!',
+  ogSiteName: 'Córdoba WebConf 2020 | ¡Estamos buscando disertantes!',
+  ogDescription: 'La primera conferencia de tecnologías Web del interior del país, presentada por CoDeAr. Te esperamos el 29 y 30 de Mayo de 2020, en el Auditorio de la UTN - Facultad Regional Córdoba. ¡Estamos buscando disertantes!',
+  ogUrl: 'https://webconf.tech',
+  ogLocale: 'es_AR',
+  ogImage: 'https://webconf.tech/icons/apple-icon-180x180.png',
+  twitterTitle: 'Córdoba WebConf 2020 | ¡Estamos buscando disertantes!',
+  twitterDescription: 'La primera conferencia de tecnologías Web del interior del país, presentada por CoDeAr. Te esperamos el 29 y 30 de Mayo de 2020, en el Auditorio de la UTN - Facultad Regional Córdoba. ¡Estamos buscando disertantes!',
+  twitterSite: '@WebConfAr',
+  twitterCreator: '@WebConfAr',
+};

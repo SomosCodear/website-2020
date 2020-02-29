a8c9c2f34d41feb97903c150a96776c84ef67777
homer0 <leo.apiwan@olapic.com>
2020-02-29 09:27:37 -0300

    fix(checkout): add the proper sizes on the textbox

--- style/lilac/components/TextBox.jsx
+++ style/lilac/components/TextBox.jsx
@@ -4,18 +4,19 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  border: 1px solid #FFFFFF;
+  border: 1px solid var(--color-text);
   border-radius: 10px;
   display: flex;
   flex-direction: column;
   margin: 0.875rem 0;
-  padding: 10px 10px 4px 10px;
+  padding: 15px 15px 4px 15px;
 `;
 const Label = styled.label`
   text-transform: uppercase;
-  color: white;
-  opacity: 0.7;
-  font-size: 14px;
+  color: var(--color-text);
+  font-weight: bold;
+  opacity: 0.6;
+  font-size: 1.125rem;
 `;
 const Input = styled.input`
   width: 100%;
@@ -25,7 +26,7 @@ const Input = styled.input`
   margin: 0;
   background: transparent;
   color: white;
-  font-size: 32px;
+  font-size: 3rem;
 `;
 
 export const TextBox = ({

5b57650ba5de5a563a7d08b754ef5b6204a9a93a
Agustin Carrasco <asermax@gmail.com>
2020-03-01 18:18:21 -0300

    feat(redux): connect to redux tools

--- data/store.js
+++ data/store.js
@@ -1,12 +1,24 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+/* globals window */
+import {
+  compose,
+  createStore,
+  combineReducers,
+  applyMiddleware,
+} from 'redux';
 import thunk from 'redux-thunk';
 import { reducers } from './reducers';
+
+const composeEnhancers = (
+  typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+) || compose;
 
 export function configureStore(initialState = {}) {
   const store = createStore(
     combineReducers(reducers),
     initialState,
-    applyMiddleware(thunk),
+    composeEnhancers(
+      applyMiddleware(thunk),
+    ),
   );
 
   return store;

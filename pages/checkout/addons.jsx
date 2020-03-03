b2b74c29c2c316809cf2cd5a5aa48f72f064ce5e
Agustin Carrasco <asermax@gmail.com>
2020-03-03 00:25:11 -0300

    feat(checkout): integrate addons logic into third step

--- pages/checkout/addons.jsx
+++ pages/checkout/addons.jsx
@@ -1,32 +1,37 @@
-import React from 'react';
+import R from 'ramda';
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useRouter } from 'next/router';
 import styled, { css } from 'styled-components';
-import { CheckoutLayout, CheckoutTitle, CheckoutActions } from '../../layouts/checkout';
+import { fetchItems } from '../../data/items/actions';
+import { ITEM_TYPE_ADDON } from '../../data/items/constants';
+import { getItemsByType } from '../../data/items/selectors';
+import { getAddons } from '../../data/order/selectors';
+import { setOrderAddonAmount } from '../../data/order/actions';
+import {
+  CheckoutLayout,
+  CheckoutTitle,
+  CheckoutActions,
+  CheckoutAction,
+} from '../../layouts/checkout';
 import { Product } from '../../components/Product';
-
-const productsData = Array.from(new Array(5), (_, index) => {
-  const i = index + 1;
-  return {
-    id: `prod${i}`,
-    name: `Remera oficial WebConf 2020 (${i})`,
-    image: '/images/shirt.png',
-    price: i * 100,
-    count: i,
-  };
-});
 
 const ProductsList = styled.ul`
   list-style: none;
   margin: 0;
   padding: 0;
 `;
+
 const ProductItem = styled.li`
   padding-bottom: 2.375rem;
 `;
+
 const DetailContainer = styled.div`
   color: var(--color-text);
   background: #00000080 0% 0%;
   padding: 0 2.125rem;
 `;
+
 const DetailTitle = styled.h2`
   font-size: 2rem;
   margin: 0;
@@ -34,16 +39,19 @@ const DetailTitle = styled.h2`
   font-weight: lighter;
   opacity: .5;
 `;
+
 const Disclaimer = styled.p`
   font-size: 0.875rem;
   margin: 0 0 1.5rem 0;
   opacity: .5;
 `;
+
 const DetailItems = styled.ul`
   list-style: none;
   margin: 0;
   padding: 0;
 `;
+
 const DetailItem = styled.li`
   padding-bottom: 0.9375rem;
   margin-bottom: 0.9375rem;
@@ -52,6 +60,7 @@ const DetailItem = styled.li`
     border-bottom: 0;
   }
 `;
+
 const DetailItemTitle = styled.div`
   display: flex;
   flex-direction: row;
@@ -59,19 +68,23 @@ const DetailItemTitle = styled.div`
   align-items: center;
   font-size: 1.5rem;
 `;
+
 const DetailItemLabel = styled.span`
   display: block;
 `;
+
 const DetailItemPrice = styled.span`
   display: block;
   ${({ bold }) => bold && css`font-weight: bold;`}
   ${({ big }) => big && css`font-size: 2rem;`}
 `;
+
 const DetailItemSubtitle = styled.p`
   opacity: .5;
   font-size: 0.875rem;
   margin: 0.25rem 0 0 0;
 `;
+
 const DetailSubItem = styled.li`
   opacity: .5;
   margin-left: 1.9375rem;
@@ -89,78 +102,104 @@ const DetailSubItem = styled.li`
   }
 `;
 
-const CheckoutSecondStep = () => (
-  <CheckoutLayout>
-    <CheckoutTitle
-      title="¿Querés algo más?"
-    />
-    <ProductsList>
-      {productsData.map((prod) => (
-        <ProductItem key={prod.id}>
-          <Product
-            name={prod.name}
-            image={prod.image}
-            price={prod.price}
-            count={prod.count}
-            onCountChange={() => {}}
-          />
-        </ProductItem>
-      ))}
-    </ProductsList>
-    <DetailContainer>
-      <DetailTitle>Resumen de cuenta</DetailTitle>
-      <Disclaimer>Todos los precios son finales y en Pesos Argentinos.</Disclaimer>
-      <DetailItems>
-        <DetailItem>
-          <DetailItemTitle>
-            <DetailItemLabel>Pase de conferencia</DetailItemLabel>
-            <DetailItemPrice bold>$ 1.200</DetailItemPrice>
-          </DetailItemTitle>
-          <DetailItemSubtitle>
-            JOEL ALEJANDRO VILLARREAL BERTOLDI
-          </DetailItemSubtitle>
-          <DetailItems>
-            <DetailSubItem>
-              <DetailItemLabel>Full x1</DetailItemLabel>
-              <DetailItemPrice>$ 1.200</DetailItemPrice>
-            </DetailSubItem>
-          </DetailItems>
-        </DetailItem>
-        <DetailItem>
-          <DetailItemTitle>
-            <DetailItemLabel>Extras</DetailItemLabel>
-            <DetailItemPrice bold>$ 2.250</DetailItemPrice>
-          </DetailItemTitle>
-          <DetailItems>
-            <DetailSubItem>
-              <DetailItemLabel>Remera x2</DetailItemLabel>
-              <DetailItemPrice>$ 1.400</DetailItemPrice>
-            </DetailSubItem>
-            <DetailSubItem>
-              <DetailItemLabel>Taza x1</DetailItemLabel>
-              <DetailItemPrice>$ 350</DetailItemPrice>
-            </DetailSubItem>
-            <DetailSubItem>
-              <DetailItemLabel>Cuaderno x1</DetailItemLabel>
-              <DetailItemPrice>$ 500</DetailItemPrice>
-            </DetailSubItem>
-          </DetailItems>
-        </DetailItem>
-        <DetailItem>
-          <DetailItemTitle>
-            <DetailItemLabel>Total a pagar</DetailItemLabel>
-            <DetailItemPrice big bold>$ 3.450</DetailItemPrice>
-          </DetailItemTitle>
-        </DetailItem>
-      </DetailItems>
+const CheckoutThirdStep = () => {
+  const router = useRouter();
+  const dispatch = useDispatch();
 
-      <CheckoutActions
-        onContinue={() => {}}
-        onGoBack={() => {}}
+  const addons = useSelector(getAddons);
+  const items = useSelector((state) => getItemsByType(state, ITEM_TYPE_ADDON));
+
+  const updateAmount = useCallback(
+    R.curry((id, amount) => dispatch(setOrderAddonAmount(id, amount))),
+    [dispatch],
+  );
+
+  return (
+    <CheckoutLayout>
+      <CheckoutTitle
+        title="¿Querés algo más?"
       />
+      <ProductsList>
+        {items.map(({
+          id,
+          name,
+          image,
+          price,
+        }) => (
+          <ProductItem key={id}>
+            <Product
+              name={name}
+              image={image}
+              price={parseInt(price, 10)}
+              count={R.compose(R.defaultTo(0), R.prop(id))(addons)}
+              onCountChange={updateAmount(id)}
+            />
+          </ProductItem>
+        ))}
+      </ProductsList>
+      <DetailContainer>
+        <DetailTitle>Resumen de cuenta</DetailTitle>
+        <Disclaimer>Todos los precios son finales y en Pesos Argentinos.</Disclaimer>
+        <DetailItems>
+          <DetailItem>
+            <DetailItemTitle>
+              <DetailItemLabel>Pase de conferencia</DetailItemLabel>
+              <DetailItemPrice bold>$ 1.200</DetailItemPrice>
+            </DetailItemTitle>
+            <DetailItemSubtitle>
+              JOEL ALEJANDRO VILLARREAL BERTOLDI
+            </DetailItemSubtitle>
+            <DetailItems>
+              <DetailSubItem>
+                <DetailItemLabel>Full x1</DetailItemLabel>
+                <DetailItemPrice>$ 1.200</DetailItemPrice>
+              </DetailSubItem>
+            </DetailItems>
+          </DetailItem>
+          <DetailItem>
+            <DetailItemTitle>
+              <DetailItemLabel>Extras</DetailItemLabel>
+              <DetailItemPrice bold>$ 2.250</DetailItemPrice>
+            </DetailItemTitle>
+            <DetailItems>
+              <DetailSubItem>
+                <DetailItemLabel>Remera x2</DetailItemLabel>
+                <DetailItemPrice>$ 1.400</DetailItemPrice>
+              </DetailSubItem>
+              <DetailSubItem>
+                <DetailItemLabel>Taza x1</DetailItemLabel>
+                <DetailItemPrice>$ 350</DetailItemPrice>
+              </DetailSubItem>
+              <DetailSubItem>
+                <DetailItemLabel>Cuaderno x1</DetailItemLabel>
+                <DetailItemPrice>$ 500</DetailItemPrice>
+              </DetailSubItem>
+            </DetailItems>
+          </DetailItem>
+          <DetailItem>
+            <DetailItemTitle>
+              <DetailItemLabel>Total a pagar</DetailItemLabel>
+              <DetailItemPrice big bold>$ 3.450</DetailItemPrice>
+            </DetailItemTitle>
+          </DetailItem>
+        </DetailItems>
+        <CheckoutActions>
+          <CheckoutAction backButton onClick={router.back} />
+          <CheckoutAction type="submit" />
+        </CheckoutActions>
+      </DetailContainer>
+    </CheckoutLayout>
+  );
+};
 
-    </DetailContainer>
-  </CheckoutLayout>
-);
+CheckoutThirdStep.getInitialProps = async ({ store, isServer }) => {
+  const promise = store.dispatch(fetchItems(ITEM_TYPE_ADDON));
 
-export default CheckoutSecondStep;
+  if (isServer) {
+    await promise;
+  }
+
+  return {};
+};
+
+export default CheckoutThirdStep;

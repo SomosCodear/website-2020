3f857627b8f7ceae8b4ad5abd739657f1fa7da91
Agustin Carrasco <asermax@gmail.com>
2020-03-18 23:40:01 -0300

    feat(config): allow dotenv per-env

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 const dotenv = require('dotenv');
 const withCSS = require('@zeit/next-css');
 
-dotenv.config();
+dotenv.config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 
 module.exports = withCSS({
   target: 'serverless',
   env: {
     API_URL: process.env.API_URL,
+    SENTRY_DSN: process.env.SENTRY_DSN,
   },
 });
